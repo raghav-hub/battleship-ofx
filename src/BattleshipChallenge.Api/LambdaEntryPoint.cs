e8eaf464c5ff281d5c85008d8087eef0c1f42feb
raghav.puthu <raghavendra.puthu@beteasy.com.au>
2021-06-10 03:28:06 +1000

    fix deployment to aws

--- src/BattleshipChallenge.Api/LambdaEntryPoint.cs
+++ src/BattleshipChallenge.Api/LambdaEntryPoint.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace BattleshipChallenge.Api
+namespace BattleshipChallengeApi
 {
     /// <summary>
     /// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the
